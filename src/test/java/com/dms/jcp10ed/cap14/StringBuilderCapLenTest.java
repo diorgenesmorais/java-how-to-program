d6dff311966f473d532afc28ba2b1f85f1fa4021
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-05-09 22:59:19 -0300

    14.4.2 Métodos StringBuilder

---
+++ src/test/java/com/dms/jcp10ed/cap14/StringBuilderCapLenTest.java
@@ -0,0 +1,18 @@
+package com.dms.jcp10ed.cap14;
+
+public class StringBuilderCapLenTest {
+
+	public static void main(String[] args) {
+
+		StringBuilder buffer = new StringBuilder("Hello, how are you?");
+		System.out.printf("buffer = %s%nlength = %d%ncapacity = %d%n%n", buffer.toString(), buffer.length(),
+				buffer.capacity());
+
+		buffer.ensureCapacity(75);
+		System.out.printf("New capacity = %d%n%n", buffer.capacity());
+
+		buffer.setLength(10);
+		System.out.printf("New length = %d%nbuffer = %s%n", buffer.length(), buffer.toString());
+	}
+
+}
